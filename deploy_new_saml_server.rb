1b675f6ba3ed64eb0dc4cfd9ba7a0527b1acb4d5
Andrew Cantino <acantino@pivotal.io>
2016-02-16 15:53:23 -0800

    rename cf_push to deploy_new_saml_server
    Signed-off-by: Maryam Labib <mlabib@pivotal.io>

---
+++ deploy_new_saml_server.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+def self.run(cmd)
+  system(cmd) or raise "Failed to run #{cmd}"
+end
+
+def prompt(string)
+  print string
+  STDOUT.flush
+  gets.strip
+end
+
+puts
+puts "Welcome #{`whoami`.strip}! Please make sure you have cf setup and logged in."
+puts
+
+opsman_ip = prompt "What IP or hostname (with https and port) would you like this SAML server to accept traffic from? "
+
+File.write('metadata/saml20-sp-remote.php', <<-PHP)
+<?php
+
+$metadata['opsman-login'] = array(
+    'AssertionConsumerService' => '#{opsman_ip}/uaa/saml/SSO/alias/opsman-login',
+    'SingleLogoutService' => '#{opsman_ip}/uaa/saml/SSO/alias/opsman-login',
+    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+    'simplesaml.nameidattribute' => 'emailAddress',
+);
+PHP
+
+name = prompt "What name should this be pushed to pws as: "
+if name.length == 0
+  puts "Aborting."
+  run 'git checkout metadata/saml20-sp-remote.php'
+  exit
+end
+
+prompt "Hit return to cf push #{name} to your default space and credentials. "
+
+run "cf push #{name} -m 128M -b https://github.com/cf-identity/php-buildpack.git"
+run 'git checkout metadata/saml20-sp-remote.php'
+
