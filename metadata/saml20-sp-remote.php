542f92ccd971d5c48efbaca91d2e96921cfcfa11
Emily Berk <eberk@vmware.com>
2020-11-04 16:33:55 -0600

    Add more service providers (dev-sso, localhost:8080 and :9000)
    - So we can do local development
    - And so that dev-sso can use this as an IDP
    
    [#175465397]
    
    Signed-off-by: Joshua Casey <cjoshua@vmware.com>

--- metadata/saml20-sp-remote.php
+++ metadata/saml20-sp-remote.php
@@ -6,3 +6,21 @@ $metadata['dev-plan.login.sfo.identity.team'] = array(
     'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
     'simplesaml.nameidattribute' => 'emailAddress',
 );
+
+$metadata['http://dev-sso-auth-server/saml2/service-provider-metadata/okta'] = array(
+    'AssertionConsumerService' => 'http://35.197.103.195:30697/login/saml2/sso/okta',
+    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+    'simplesaml.nameidattribute' => 'emailAddress',
+);
+
+$metadata['http://localhost:8080/saml2/service-provider-metadata/okta'] = array(
+    'AssertionConsumerService' => 'http://localhost:8080/login/saml2/sso/okta',
+    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+    'simplesaml.nameidattribute' => 'emailAddress',
+);
+
+$metadata['http://localhost:9000/saml2/service-provider-metadata/okta'] = array(
+    'AssertionConsumerService' => 'http://localhost:9000/login/saml2/sso/okta',
+    'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+    'simplesaml.nameidattribute' => 'emailAddress',
+);
